14d2c73b17aaaa4bc5d163810b69e96c79295afc
Anton Kvasha <amkvasha@edu.hse.ru>
2020-03-05 17:17:05 +0300

    Homework-1 init commit

---
+++ documentation.md
@@ -0,0 +1,30 @@
+# Документация
+## Объекты
+### Product
+* **Описание**: Содержит информацию о товаре
+* **Поля**:
+    * **Title (string)**: Название товара
+    * **ID (uint64)**: Код товара. Является уникальным идентификатором товара.
+    * **Category (string)**: Категория товара
+    
+## Методы
+### createProduct
+* **Описание**: Добавляет товар в список 
+* **HttpMethod**: POST
+* **URL**: /products
+### deleteProduct
+* **Описание**: Удаляет товар с указанным ID из списка 
+* **HttpMethod**: DELETE
+* **URL**: /products/{id}
+### getProducts
+* **Описание**: Возвращает список всех товаров
+* **HttpMethod**: GET
+* **URL**: /products
+### getProduct
+* **Описание**: Возвращает товар с указанным ID
+* **HttpMethod**: GET
+* **URL**: /products/{id}
+### updateProduct
+* **Описание**: Обновляет информацию о товаре с указанным ID
+* **HttpMethod**: PUT
+* **URL**: /products/{id}
---
+++ main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"sync"
+)
+
+type Product struct {
+	Title    string `json:"title"`
+	ID       string `json:"id"`
+	Category uint64 `json:"category"`
+}
+
+var products []Product
+var mutex sync.RWMutex
+
+func createProduct(w http.ResponseWriter, r *http.Request) {
+	mutex.RLock()
+	defer mutex.RUnlock()
+	w.Header().Set("Content-Type", "application/json")
+	var product Product
+	if err := json.NewDecoder(r.Body).Decode(&product); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	for _, currProduct := range products {
+		if currProduct.ID == product.ID {
+			http.Error(w, "Item with such ID already exists!", http.StatusBadRequest)
+			return
+		}
+	}
+	products = append(products, product)
+	_ = json.NewEncoder(w).Encode(products)
+}
+
+func deleteProduct(w http.ResponseWriter, r *http.Request) {
+	mutex.RLock()
+	defer mutex.RUnlock()
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for i, product := range products {
+		if product.ID == params["id"] {
+			products = append(products[:i], products[i+1:]...)
+			return
+		}
+	}
+	http.Error(w, "Item with given ID not found!", http.StatusBadRequest)
+}
+
+func getProducts(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	_ = json.NewEncoder(w).Encode(products)
+}
+
+func getProduct(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, product := range products {
+		if product.ID == params["id"] {
+			if err := json.NewEncoder(w).Encode(product); err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+			return
+		}
+	}
+	http.Error(w, "Item with given ID not found!", http.StatusBadRequest)
+}
+
+func updateProduct(w http.ResponseWriter, r *http.Request) {
+	mutex.RLock()
+	defer mutex.RUnlock()
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for i, product := range products {
+		if product.ID == params["id"] {
+			var newProduct Product
+			if err := json.NewDecoder(r.Body).Decode(&newProduct); err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+			newProduct.ID = product.ID
+			products = append(products[:i], products[i+1:]...)
+			products = append(products, newProduct)
+			_ = json.NewEncoder(w).Encode(product)
+			return
+		}
+	}
+	http.Error(w, "Item with given ID not found!", http.StatusBadRequest)
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/products", createProduct).Methods("POST")
+	r.HandleFunc("/products/{id}", deleteProduct).Methods("DELETE")
+	r.HandleFunc("/products", getProducts).Methods("GET")
+	r.HandleFunc("/products/{id}", getProduct).Methods("GET")
+	r.HandleFunc("/products/{id}", updateProduct).Methods("PUT")
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
